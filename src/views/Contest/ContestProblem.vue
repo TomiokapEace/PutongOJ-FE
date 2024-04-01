15003e4eacaad30827426c2d1cc820ce26f734ef
TomiokapEace <1256118542@qq.com>
2024-04-01 19:39:18 +0800

    新增 课程题目页面将已通过的题号按钮背景色变为绿色

--- src/views/Contest/ContestProblem.vue
+++ src/views/Contest/ContestProblem.vue
@@ -6,10 +6,6 @@ import Problem from '@/components/Problem'
 import { useProblemStore } from '@/store/modules/problem'
 import { useContestStore } from '@/store/modules/contest'
 import { onRouteParamUpdate } from '@/util/helper'
-// import { reactive, ref } from 'vue'
-// import { ECharts as Chart } from 'vue-echarts'
-// import 'echarts/lib/chart/pie'
-// import 'echarts/lib/component/title'
 
 const { t } = useI18n()
 const problemStore = useProblemStore()
@@ -19,7 +15,7 @@ const route = useRoute()
 
 const { findOne: findOneProblem } = problemStore
 const { problem } = $(storeToRefs(problemStore))
-const { overview, contest, totalProblems } = $(storeToRefs(contestStore))
+const { overview, contest, totalProblems, solved } = $(storeToRefs(contestStore))
 
 const proIndex = $computed(() => Number.parseInt(route.params.id || 1))
 
@@ -30,46 +26,6 @@ function fetch () {
 const pageChange = val => router.push({ name: 'contestProblem', params: { id: val } })
 const submit = () => router.push({ name: 'contestSubmit', params: router.params })
 
-// const pieOption = reactive({
-//   title: {
-//     text: 'Referer of a Website',
-//     subtext: 'Fake Data',
-//     left: 'center'
-//   },
-//   tooltip: {
-//     trigger: 'item'
-//   },
-//   legend: {
-//     orient: 'vertical',
-//     left: 'left'
-//   },
-//   series: [
-//     {
-//       name: 'Access From',
-//       type: 'pie',
-//       radius: '50%',
-//       data: [
-//         { value: 100, name: 'CE' },
-//         { value: 1000, name: 'AC' },
-//         { value: 1000, name: 'RE' },
-//         { value: 1000, name: 'WA' },
-//         { value: 1000, name: 'TLE' },
-//         { value: 1000, name: 'MLE' },
-//         { value: 1000, name: 'OLE' },
-//         { value: 1000, name: 'PE' },
-//         { value: 1000, name: 'SE' },
-//       ],
-//       emphasis: {
-//         itemStyle: {
-//           shadowBlur: 10,
-//           shadowOffsetX: 0,
-//           shadowColor: 'rgba(0, 0, 0, 0.5)'
-//         }
-//       }
-//     }
-//   ]
-// })
-
 fetch()
 onRouteParamUpdate(fetch)
 </script>
@@ -77,7 +33,7 @@ onRouteParamUpdate(fetch)
 <template>
   <div class="conpro-wrap">
     <ul>
-      <li v-for="i in totalProblems" :key="i" :class="{ active: i === proIndex }" @click="pageChange(i)">
+      <li v-for="i in totalProblems" :key="i" :class="{ active: i === proIndex, solved: solved.includes(overview[i - 1].pid) }" @click="pageChange(i)">
         {{ i }}
       </li>
     </ul>
@@ -89,13 +45,6 @@ onRouteParamUpdate(fetch)
     <Button shape="circle" icon="md-paper-plane" @click="submit">
       {{ t('oj.submit') }}
     </Button>
-    <!-- <div class="chart-container">
-      <Chart v-if="isAdmin" :option="pieOption" auto-resize />
-    </div> -->
-    <!-- .chart-container {
-      height: 400px;
-      margin-top: 20px;
-    } -->
   </div>
 </template>
 
@@ -119,4 +68,8 @@ ul
     color: #fff
     background-color: #e040fb
     border-color: #e040fb
+  li.solved {
+    background-color: #A0D468;
+    color: #fff;
+  }
 </style>
